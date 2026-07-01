828214d37ba5803e908a2d3c969355a8d56737c0
Johanny_Amaya <jamayau@unal.edu.co>
2026-07-01 05:06:39 -0500

    Agregar DriverManager para gestion del WebDriver

---
+++ src/test/java/com/makers/utils/DriverManager.java
@@ -0,0 +1,71 @@
+package com.makers.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+
+import java.time.Duration;
+
+public class DriverManager {
+
+    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+        if (driver.get() == null) {
+            initDriver();
+        }
+        return driver.get();
+    }
+
+    private static void initDriver() {
+        String browser  = ConfigReader.getBrowser().toLowerCase();
+        boolean headless = ConfigReader.isHeadless();
+        int implicitWait = ConfigReader.getImplicitWait();
+
+        WebDriver webDriver;
+
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions ffOptions = new FirefoxOptions();
+                if (headless) ffOptions.addArguments("--headless");
+                webDriver = new FirefoxDriver(ffOptions);
+                break;
+
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                if (headless) edgeOptions.addArguments("--headless");
+                webDriver = new EdgeDriver(edgeOptions);
+                break;
+
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                if (headless) {
+                    chromeOptions.addArguments("--headless");
+                    chromeOptions.addArguments("--no-sandbox");
+                    chromeOptions.addArguments("--disable-dev-shm-usage");
+                }
+                webDriver = new ChromeDriver(chromeOptions);
+                break;
+        }
+
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWait));
+        webDriver.manage().window().maximize();
+        driver.set(webDriver);
+    }
+
+    public static void quitDriver() {
+        if (driver.get() != null) {
+            driver.get().quit();
+            driver.remove();
+        }
+    }
+}
